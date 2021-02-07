3731c9b1e1f3bb5e184a8bd287b0c4d92e2c37b4
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-02-07 08:21:55 -0300

    Catalog loaders finished

--- front-web/src/pages/Catalog/components/ProductDetails/index.tsx
+++ front-web/src/pages/Catalog/components/ProductDetails/index.tsx
@@ -1,53 +1,66 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { ReactComponent as ArrowIcon } from '../../../../core/assets/images/arrow.svg';
+import { ReactComponent as ArrowIcon } from 'core/assets/images/arrow.svg';
 import ProductPrice from '../../../../core/components/ProductPrice';
 import { Product } from '../../../../core/types/Product';
 import { makeRequest } from '../../../../core/utils/request';
+import ProductDescriptionLoader from '../Loaders/ProductDescriptionLoader';
+import ProductInfoLoader from '../Loaders/ProductInfoLoader';
 import './styles.scss';
 
 type ParamsType = {
-    productId: string;
+  productId: string;
 }
 
 const ProductDetails = () => {
-    const { productId } = useParams<ParamsType>();
-    const [product, setProduct] = useState<Product>();
-    
-    useEffect(() => {
-        makeRequest({ url: `/products/${productId}` })
-        .then(response => setProduct(response.data));
-    }, [productId]);
+  const { productId } = useParams<ParamsType>();
+  const [product, setProduct] = useState<Product>();
+  const [isLoading, setIsLoading] = useState(false);
 
-    return (
-        <div className="product-details-container">
-            <div className="card-base-20 product-details">  
-                <Link to="/products" className="product-details-go-back">
-                    <ArrowIcon className="icon-go-back" />
-                    <h1 className="text-go-back">voltar</h1>
-                </Link>
-                <div className="row">
-                    <div className="col-6 pr-5">
-                        <div className="product-details-card text-center">
-                            <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
-                        </div>
-                        <h1 className="product-details-name">
-                            {product?.name}
-                        </h1>
-                        { product?.price && <ProductPrice price={product?.price}/>}            
-                    </div>
-                    <div className="col-6 product-details-card">
-                        <h1 className="product-description-title">
-                            Descrição do Produto
-                        </h1> 
-                        <p className="product-description-paragraph">
-                        {product?.description}
-                        </p>
-                    </div>
+  useEffect(() => {
+    setIsLoading(true);
+    makeRequest({ url: `/products/${productId}` })
+      .then(response => setProduct(response.data))
+      .finally(() => setIsLoading(false));
+  }, [productId]);
+
+  return (
+    <div className="product-details-container">
+      <div className="card-base-20 product-details">
+        <Link to="/products" className="product-details-go-back">
+          <ArrowIcon className="icon-go-back" />
+          <h1 className="text-go-back">voltar</h1>
+        </Link>
+        <div className="row">
+          <div className="col-6 pr-5">
+            {isLoading ? <ProductInfoLoader /> : (
+              <>
+                <div className="product-details-card text-center">
+                  <img src={product?.imgUrl} alt={product?.name} className="product-details-image" />
                 </div>
-            </div>
+                <h1 className="product-details-name">
+                  {product?.name}
+                </h1>
+                {product?.price && <ProductPrice price={product?.price} />}
+              </>
+            )}
+          </div>
+          <div className="col-6 product-details-card">
+            {isLoading ? <ProductDescriptionLoader /> : (
+              <>
+                <h1 className="product-description-title">
+                  Descrição do Produto
+                </h1>
+                <p className="product-description-paragraph">
+                  {product?.description}
+                </p>
+              </>
+            )}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
