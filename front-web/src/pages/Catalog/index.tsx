d034450eacef07f1a5ad0e9e1c4100366cad0a78
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-04-19 13:59:28 -0300

    Docker added

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
