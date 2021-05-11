29113ee9a32da0e34c27e518c9cc30b97fe59c36
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-05-10 22:28:25 -0300

    React hook form and toastfy added

--- front-web/src/pages/Admin/components/Products/Form/index.tsx
+++ front-web/src/pages/Admin/components/Products/Form/index.tsx
@@ -1,88 +1,94 @@
+import React from 'react';
 import { makePrivateRequest } from 'core/utils/request';
-import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { toast } from 'react-toastify';
+import { useHistory } from 'react-router';
 import BaseForm from '../../BaseForm';
 import './styles.scss';
 
 type FormState = {
   name: string;
   price: string;
-  category: string;
   description: string;
+  imageUrl: string;
 }
 
-type FormEvent = React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>;
-
 const Form = () => {
-  const [formData, setFormData] = useState<FormState>({
-    name: '',
-    price: '',
-    category: '1',
-    description: ''
-  });
+  const { register, handleSubmit, formState: { errors } } = useForm<FormState>();
+  const history = useHistory();
 
-  const handleOnChange = (event: FormEvent) => {
-    const name = event.target.name;
-    const value = event.target.value;
-
-    setFormData(data => ({ ...data, [name]: value }));
-  }
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    const payload = {
-      ...formData,
-      imgUrl: 'https://pricespy-75b8.kxcdn.com/product/standard/280/5113846.jpg',
-      categories: [{ id: formData.category }]
-    }
-
-    makePrivateRequest({ url: '/products', method: 'POST', data: payload })
-      .then(() => {
-        setFormData({ name: '', category: '', price: '', description: '' })
-
-      });
+  const onSubmit = (data: FormState) => {
+    makePrivateRequest({ url: '/products', method: 'POST', data })
+    .then(() => {
+      toast.info('Produto salvo com sucesso!');
+      history.push('/admin/products');
+    })
+    .catch(() => {
+      toast.error('Erro ao salvar o produto');
+    })
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <BaseForm title="cadastrar um produto">
         <div className="row">
           <div className="col-6">
-            <input
-              value={formData.name}
-              name="name"
-              type="text"
-              className="form-control mb-5"
-              onChange={handleOnChange}
-              placeholder="Nome do produto"
-            />
-            <select
-              value={formData.category}
-              className="form-control mb-5"
-              onChange={handleOnChange}
-              name="category"
-            >
-              <option value="1">Livros</option>
-              <option value="3">Computadores</option>
-              <option value="2">Eletrônicos</option>
-            </select>
-            <input
-              value={formData.price}
-              name="price"
-              type="text"
-              className="form-control mb-5"
-              onChange={handleOnChange}
-              placeholder="Preço do produto"
-            />
+            <div className="margin-bottom-30">
+              <input
+                {...register('name', { required: "Campo obrigatório",
+                minLength: { value: 5, message: 'O campo deve ter no mínimo 5 caracteres'},
+                maxLength: { value: 60, message: 'O campo deve ter no máximo 60 caracteres'},
+                })}
+                type="text"
+                className="form-control input-base"
+                placeholder="Nome do produto"
+              />
+              {errors.name && (
+                <div className="invalid-feedback d-block">
+                  {errors.name.message}
+                </div>
+              )}
+            </div>
+            <div className="margin-bottom-30">
+              <input
+                {...register('price', { required: "Campo obrigatório" })}
+                type="number"
+                className="form-control input-base"
+                placeholder="Preço do produto"
+              />
+              {errors.price && (
+                <div className="invalid-feedback d-block">
+                  {errors.price.message}
+                </div>
+              )}
+            </div>
+            <div className="margin-bottom-30">
+              <input
+                {...register('imageUrl', { required: "Campo obrigatório" })}
+                type="text"
+                className="form-control input-base"
+                placeholder="Imagem do produto"
+              />
+              {errors.imageUrl && (
+                <div className="invalid-feedback d-block">
+                  {errors.imageUrl.message}
+                </div>
+              )}
+            </div>
           </div>
           <div className="col-6">
             <textarea
-              name="description"
-              value={formData.description}
-              onChange={handleOnChange}
-              className="form-control"
+              {...register('description', { required: "Campo obrigatório" })}
+              className="form-control input-base"
+              placeholder="Descrição"
               cols={30}
               rows={10}
             />
+            {errors.description && (
+              <div className="invalid-feedback d-block">
+                {errors.description.message}
+              </div>
+            )}
           </div>
         </div>
       </BaseForm>
