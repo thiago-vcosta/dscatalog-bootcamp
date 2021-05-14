4b83c89ac500195a504050c3fd4e871698b7e732
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-05-14 08:46:05 -0300

    finish admin product card layout

--- front-web/src/pages/Catalog/components/ProductCard/index.tsx
+++ front-web/src/pages/Catalog/components/ProductCard/index.tsx
@@ -1,22 +1,22 @@
 import React from 'react';
-import ProductPrice from '../../../../core/components/ProductPrice';
-import { Product } from '../../../../core/types/Product';
+import ProductPrice from 'core/components/ProductPrice';
+import { Product } from 'core/types/Product';
 import './styles.scss';
 
 type Props = {
-    product: Product;
+  product: Product;
 }
 
 const ProductCard = ({ product }: Props) => (
-        <div className="card-base-10 product-card">
-            <img src={product.imgUrl} alt={product.name} className="product-card-image" />
-            <div className="product-info">
-                <h6 className="product-name">
-                {product.name}
-                </h6>
-                <ProductPrice price={product.price}/>
-            </div>
-        </div>
-    );
+  <div className="card-base-10 product-card">
+    <img src={product.imgUrl} alt={product.name} className="product-card-image" />
+    <div className="product-info">
+      <h6 className="product-name">
+        {product.name}
+      </h6>
+      <ProductPrice price={product.price} />
+    </div>
+  </div>
+);
 
 export default ProductCard;
