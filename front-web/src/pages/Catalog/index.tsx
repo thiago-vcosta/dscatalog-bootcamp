984f116a13923d2cb79431aa7c409fdbf9f6583e
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-05-18 08:30:40 -0300

    Finish products filter

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductsResponse } from 'core/types/Product';
 import { makeRequest } from 'core/utils/request';
@@ -7,54 +6,60 @@ import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
 import Pagination from 'core/components/Pagination';
-import ProductFilters from 'core/components/ProductFilters';
+import ProductFilters, { FilterForm } from 'core/components/ProductFilters';
 
-const Catalog = () => { 
-    const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
-    const [isLoading, setIsLoading] = useState(false);
-    const [activePage, setActivePage] = useState(0);
+const Catalog = () => {
+  const [productsResponse, setProductsResponse] = useState<ProductsResponse>();
+  const [isLoading, setIsLoading] = useState(false);
+  const [activePage, setActivePage] = useState(0);
 
-    useEffect(() => {
-        const params = {
-        page: activePage,
-        linesPerPage: 12
-        }
+  const getProducts = useCallback((filter?: FilterForm) => {
+    const params = {
+      page: activePage,
+      linesPerPage: 12,
+      name: filter?.name,
+      categoryId: filter?.categoryId
+    }
 
     setIsLoading(true);
     makeRequest({ url: '/products', params })
-        .then(response => setProductsResponse(response.data))
-        .finally(() => {
-            setIsLoading(false);
-        })
-    }, [activePage]);
+      .then(response => setProductsResponse(response.data))
+      .finally(() => {
+        setIsLoading(false);
+      })
+  },[activePage]);
 
-    return (
-        <div className="catalog-container">
-            <div className="d-flex justify-content-between">
-            <h1 className="catalog-title">
-                Catálogo de produtos
+  useEffect(() => {
+    getProducts();
+  }, [getProducts]);
+
+  return (
+    <div className="catalog-container">
+      <div className="d-flex justify-content-between">
+        <h1 className="catalog-title">
+          Catálogo de produtos
             </h1>
-            <ProductFilters />
-            </div>
-            
-            <div className="catalog-products">
-                {isLoading ? <ProductCardLoader /> : (
-                    productsResponse?.content.map(product => (
-                        <Link to={`/products/${product.id}`} key={product.id}>
-                            <ProductCard product={product}/>
-                        </Link>
-                    ))
-                )}
-            </div>
-            {productsResponse && (
-              <Pagination
-                totalPages={productsResponse.totalPages}
-                activePage={activePage}
-                onChange={page => setActivePage(page)}
-                />
-            )}
-        </div>
-    );
+        <ProductFilters onSearch={filter => getProducts(filter)} />
+      </div>
+
+      <div className="catalog-products">
+        {isLoading ? <ProductCardLoader /> : (
+          productsResponse?.content.map(product => (
+            <Link to={`/products/${product.id}`} key={product.id}>
+              <ProductCard product={product} />
+            </Link>
+          ))
+        )}
+      </div>
+      {productsResponse && (
+        <Pagination
+          totalPages={productsResponse.totalPages}
+          activePage={activePage}
+          onChange={page => setActivePage(page)}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Catalog;
